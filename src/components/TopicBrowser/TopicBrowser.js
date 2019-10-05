725a2bc0f8a70b7c4c8eb4d9acd3fa7670d61f89
Jesse Badgley <jessebadgley@Jesses-MacBook-Air.local>
2019-10-05 12:47:45 -0500

    finished

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,23 +1,21 @@
-import React, {componant} from "react";
-
+import React, { Component } from 'react';
 import EvenAndOdd from '../Topics/EvenAndOdd'
 import FilterObject from '../Topics/FilterObject'
 import FilterString from '../Topics/FilterString'
-import Palindrome from '../Topics/Palindrom'
+import Palindrome from '../Topics/Palindrome'
 import Sum from '../Topics/Sum'
 
-export default class TopicBrowser extends componant {
-    render() {
-        return(
-            <div>
-                <EvenAndOdd />
-                <FilterObject />
-                <FilterString />
-                <Palindrome />
-                <Sum />
-            </div>
-
-        )
-    }
+export default class TopicBrowser extends Component {
+  render() {
+    return (
+      <div>
+        <EvenAndOdd />
+        <FilterObject />
+        <FilterString />
+        <Palindrome />
+        <Sum />
+      </div>
+    )
+  }
 }
 
